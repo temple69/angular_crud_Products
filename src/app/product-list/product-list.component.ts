fabd8442649d03d9c83d65ce391bede1b8f3c9e5
temple69 <utemple169@gmail.com>
2023-05-01 19:13:01 +0100

    Component that renders the product-list view

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ProductsService } from '../products.service';
+
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  constructor(private productsService: ProductsService){}
+  
+
+}
