c0654d6be953857f471e4931f9733327d3ea2a95
temple69 <utemple169@gmail.com>
2023-05-01 19:11:48 +0100

    Component that renders the create product view

---
+++ src/app/create-edit-products/create-edit-products.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { ProductsService } from '../products.service';
+import { v4 as uuidv4 } from 'uuid';
+import { ProductsList } from '../products';
+
+@Component({
+  selector: 'app-create-edit-products',
+  templateUrl: './create-edit-products.component.html',
+  styleUrls: ['./create-edit-products.component.css'],
+})
+export class CreateEditProductsComponent {
+  productForm = this.productsValue.group({
+    name: '',
+    description: '',
+    price: 0,
+    imgUrl: '',
+  });
+
+  constructor(
+    private productsValue: FormBuilder,
+    private productsService: ProductsService
+  ) {}
+
+  onSubmit() {
+    const name: string = this.productForm.value.name!;
+    const description: string = this.productForm.value.name!;
+    const price: number = this.productForm.value.price!;
+    const imgUrl: string = this.productForm.value.imgUrl!;
+    let productsData: ProductsList = {
+      name: name,
+      description,
+      price,
+      imgUrl,
+      id: uuidv4(),
+    };
+    this.productsService.addProducts(productsData);
+  }
+}
