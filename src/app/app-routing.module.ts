0ee1cc48bd7a8ab240ae22c58254828836b1de30
temple69 <utemple169@gmail.com>
2023-05-01 19:25:19 +0100

    configuration for angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {component:ProductListComponent,path:'product'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
