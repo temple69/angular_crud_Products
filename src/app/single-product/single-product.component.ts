42acfe7f83eaa5809247d4a1c5141550b170aa62
temple69 <utemple169@gmail.com>
2023-05-05 14:53:57 +0100

    created a component that renders single product component

--- src/app/single-product/single-product.component.ts
+++ src/app/single-product/single-product.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ProductsService } from '../service/Products/products.service';
+import{ProductsList} from '../service/Products/products'
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-single-product',
   templateUrl: './single-product.component.html',
   styleUrls: ['./single-product.component.css']
 })
-export class SingleProductComponent {
+export class SingleProductComponent implements OnInit {
+  
+  product:ProductsList={
+    productName:'',
+    price:0,
+    description:'',
+    imgUrl:''
+  };
+  constructor(private singleService:ProductsService,private route:ActivatedRoute){}
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params=>{
+      const id =params.get('productId')!
+      const single= this.singleService.products.filter(prod=>prod.id==id)
+      const [Productobj]= single
+      this.product= Productobj
+    })
+      
+  }
 
 }
