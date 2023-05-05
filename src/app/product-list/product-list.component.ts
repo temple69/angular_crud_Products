b973a12509d541afae1f9c9a22e2c99bad5ae95a
temple69 <utemple169@gmail.com>
2023-05-05 14:49:01 +0100

    updated and modified some states

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-import { ProductsService } from '../service/products.service';
-
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../service/Products/products.service';
+import { ProductsList } from '../service/Products/products';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
-  constructor(private productsService: ProductsService){}
-  
+export class ProductListComponent implements OnInit {
+  constructor(private productsService: ProductsService) {}
+  products: ProductsList[] = [];
 
+  ngOnInit(): void {
+    this.productsService.getProducts();
+    this.productsService
+      .getProductsUpdated()
+      .subscribe((ProductData: ProductsList[]) => {
+        this.products = ProductData;
+      });
+  }
+  
+  deleteProduct(id:string){
+    this.productsService.deleteProductsById(id)
+  }
 }
