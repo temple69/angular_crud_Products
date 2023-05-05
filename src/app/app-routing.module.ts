dea40e71459f22fdc48457f036e34de9cf204789
temple69 <utemple169@gmail.com>
2023-05-05 14:55:16 +0100

    updated routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { CreateEditProductsComponent } from './create-edit-products/create-edit-products.component';
+import { SingleProductComponent } from './single-product/single-product.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+
+
+
+
+
 
 
 const routes: Routes = [
-
-  {component:ProductListComponent,path:'product'}
+  {component:CreateEditProductsComponent,path:'create-product'},
+  {component:ProductListComponent,path:'products'},
+  {component:SingleProductComponent,path:'products/:productId'},
+  {component:CreateEditProductsComponent,path:'editProduct/:productId'},
+  {component:RegisterComponent,path:'register'},
+  {component:LoginComponent,path:'login'}
 ];
 
 @NgModule({
