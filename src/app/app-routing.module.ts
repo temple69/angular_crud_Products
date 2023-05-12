0208ddf799fda0e0a26d7011ce02162257c5f5b1
temple69 <utemple169@gmail.com>
2023-05-12 18:54:45 +0100

    Routing:Added route Guards to redirect user if not authenticated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TodoListComponent } from './todo-list/todo-list.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CreateEditProductsComponent } from './create-edit-products/create-edit-products.component';
 import { SingleProductComponent } from './single-product/single-product.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-
-
-
-
-
-
+import { RegisterComponent } from './Authentication/register/register.component';
+import { LoginComponent } from './Authentication/login/login.component';
+import { AuthGuard } from './service/Auth/auth.guards';
 
 const routes: Routes = [
-  {component:CreateEditProductsComponent,path:'create-product'},
-  {component:ProductListComponent,path:'products'},
-  {component:SingleProductComponent,path:'products/:productId'},
-  {component:CreateEditProductsComponent,path:'editProduct/:productId'},
-  {component:RegisterComponent,path:'register'},
-  {component:LoginComponent,path:'login'}
+  {
+    component: CreateEditProductsComponent,
+    path: 'create-product',
+    canActivate: [AuthGuard],
+  },
+  { component: ProductListComponent, path: 'products' },
+  { component: ProductListComponent, path: '' },
+  { component: SingleProductComponent, path: 'products/:productId' },
+  {
+    component: CreateEditProductsComponent,
+    path: 'editProduct/:productId',
+    canActivate: [AuthGuard],
+  },
+  { component: RegisterComponent, path: 'register' },
+  { component: LoginComponent, path: 'login' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
