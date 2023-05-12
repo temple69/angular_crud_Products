eb03f87e9083c2d576e920e18ca034a14148f8cc
temple69 <utemple169@gmail.com>
2023-05-12 18:40:12 +0100

    component:updated the product-list to update and show values dynamically based on if user is authenticated

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnDestroy } from '@angular/core';
 import { ProductsService } from '../service/Products/products.service';
 import { ProductsList } from '../service/Products/products';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../service/Auth/auth.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit {
-  constructor(private productsService: ProductsService) {}
+export class ProductListComponent implements OnInit,OnDestroy {
+  constructor(
+    private productsService: ProductsService,
+    private authService: AuthService
+  ) {}
   products: ProductsList[] = [];
+  public authProductSubs?: Subscription;
+  public isUserAuthenticated: boolean = false;
 
   ngOnInit(): void {
     this.productsService.getProducts();
@@ -18,9 +25,19 @@ export class ProductListComponent implements OnInit {
       .subscribe((ProductData: ProductsList[]) => {
         this.products = ProductData;
       });
+      this.isUserAuthenticated=this.authService.getIsAuthenticated()
+    this.authProductSubs = this.authService
+      .getAuthenticationStatusListener()
+      .subscribe((userAuthenticated) => {
+        this.isUserAuthenticated = userAuthenticated;
+      });
+  }
+
+  deleteProduct(id: string) {
+    this.productsService.deleteProductsById(id);
+  }
+  ngOnDestroy(): void {
+      this.authProductSubs?.unsubscribe()
   }
   
-  deleteProduct(id:string){
-    this.productsService.deleteProductsById(id)
-  }
 }
