702378ee7eabef32eed64a1eaedeab9db496fba3
temple69 <utemple169@gmail.com>
2023-05-12 18:33:49 +0100

    interceptor:handles the loading spinner for every request to the server

---
+++ src/app/Interceptors/loading.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, finalize } from 'rxjs';
+import { LoadingService } from '../service/Loader/loading.service';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+
+  constructor(private loader:LoadingService) {}
+  private totalRequests = 0;
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    console.log('caught')
+    this.totalRequests++;
+    this.loader.setLoading(true)
+    return next.handle(request).pipe(finalize(()=>{
+      this.totalRequests--
+      if (this.totalRequests===0) {
+        this.loader.setLoading(false);
+        
+      }
+    }));
+  }
+}
