e627159d6bd8535aefc566f09db5a792ea6d3d2c
temple69 <utemple169@gmail.com>
2023-05-12 18:37:47 +0100

    component:updated the navbar to update and show values dynamically based on if user is authenticated

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,OnDestroy } from '@angular/core';
+import { AuthService } from '../service/Auth/auth.service';
+import { Subscription } from 'rxjs';
+
+
+
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit,OnDestroy {
+  public authListenerSubs?: Subscription;  
+  public isUserAuthenticated:boolean=false
+  constructor(private authService:AuthService){}
+  ngOnInit(): void {
+    this.isUserAuthenticated=this.authService.getIsAuthenticated()
+    this.authListenerSubs=this.authService.getAuthenticationStatusListener().subscribe(userAuthenticated=>{
+      this.isUserAuthenticated=userAuthenticated
+    })
+      
+  }
+  ngOnDestroy(): void {
+      this.authListenerSubs?.unsubscribe()
+  }
+  OnLogout(){
+    this.authService.logout()
+  
+  }
 
 }
