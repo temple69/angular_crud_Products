d30dec7d146f0b734bc53125d5827037353bdb93
temple69 <utemple169@gmail.com>
2023-05-12 18:30:28 +0100

    component:refactored and did form validation renders the product creation form

--- src/app/create-edit-products/create-edit-products.component.ts
+++ src/app/create-edit-products/create-edit-products.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { AbstractControl, FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { ProductsService } from '../service/Products/products.service';
 
 import { ProductsList } from '../service/Products/products';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-create-edit-products',
@@ -15,9 +17,11 @@ export class CreateEditProductsComponent implements OnInit {
   constructor(
     private productsValue: FormBuilder,
     private productsService: ProductsService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
-  public editMode:boolean =false ;
+  
+  public editMode: boolean = false;
   productId: string = '';
   singleProduct: {} = {};
   ngOnInit(): void {
@@ -25,11 +29,13 @@ export class CreateEditProductsComponent implements OnInit {
       if (paramMap.has('productId')) {
         this.editMode = true;
         this.productId = paramMap.get('productId')!;
-        this.singleProduct = this.productsService.getSingleProduct(
-          this.productId
-        );
-        this.productForm.patchValue(this.singleProduct);
-        console.log(this.singleProduct);
+        this.productsService
+          .getSingleProduct(this.productId)
+          .subscribe((productData) => {
+            this.singleProduct = productData.product;
+            this.productForm.patchValue(this.singleProduct);
+            
+          });
       } else {
         this.editMode = false;
         this.productId = '';
@@ -37,35 +43,44 @@ export class CreateEditProductsComponent implements OnInit {
     });
   }
   productForm = this.productsValue.group({
-    productName: '',
-    description: '',
-    price: 0,
-    imgUrl: '',
-    id:''
+    productName: ['',[Validators.required,Validators.minLength(3),Validators.maxLength(20)]],
+    description: ['',[Validators.required,Validators.minLength(6),Validators.maxLength(90)]],
+    price: [0,[Validators.required,Validators.minLength(1),Validators.maxLength(6)]],
+    imgUrl:['',[Validators.required,Validators.pattern('^(https?://).+\.(jpe?g|png|svg)$')]],
+    id: '',
   });
+  submitted = false
+  
 
   onSubmit() {
+    this.submitted=true
+    if (this.productForm.invalid) {
+      return
+    }
     const productName: string = this.productForm.value.productName!;
     const description: string = this.productForm.value.description!;
     const price: number = this.productForm.value.price!;
-    const imgUrl: string = this.productForm.value.imgUrl!;
-    const id:string=this.productForm.value.id!
+    const imgUrl:string= this.productForm.value.imgUrl!;
+    const id: string = this.productForm.value.id!;
     let productsData: ProductsList = {
       productName,
       description,
       price,
       imgUrl,
-      id
+      id,
     };
     if (this.editMode) {
       this.productsService.updateProduct(productsData);
       
-    }
-    else{
-      this.productsService.addProducts(productsData);
-      console.log(this.productForm.value);
-
-    }
     
+    } else {
+      this.productsService.addProducts(productsData);
+    
+    }
+    this.productForm.reset();
+    this.router.navigateByUrl('/products')
+  }
+  get f(): { [key: string]: AbstractControl } {
+    return this.productForm.controls;
   }
 }
